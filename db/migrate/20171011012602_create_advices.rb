fa266d31f65d4cbca2c9fe4537e15b8cbbfc4a67
Kenny Zhang <Kenny.y.zhang@gmail.com>
2017-10-10 18:26:43 -0700

    update ruby version and add advice model

---
+++ db/migrate/20171011012602_create_advices.rb
@@ -0,0 +1,9 @@
+class CreateAdvices < ActiveRecord::Migration[5.1]
+  def change
+    create_table :advices do |t|
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/advices.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :advice do
+    user nil
+  end
+end
