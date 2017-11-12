10b8581322915692b837bf96018c1ec481257995
Kenny Zhang <Kenny.y.zhang@gmail.com>
2017-11-12 14:35:28 -0800

    added basic controller methods

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -5,4 +5,20 @@ class FieldsController < ApplicationController
 
   def new
   end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
