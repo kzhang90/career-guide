fe8ff62f4f3386f8204e3d5af0257dcdd8823012
Kenny Zhang <Kenny.y.zhang@gmail.com>
2017-11-22 12:44:41 -0800

    added bootstrap rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
